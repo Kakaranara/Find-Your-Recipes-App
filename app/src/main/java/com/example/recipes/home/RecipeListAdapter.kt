6753c81f2246c9ba9d1e702b1da293511470b55a
Wahyu <wahyukoco535@gmail.com>
2022-12-26 21:37:02 +0700

    recycler adapter fix + example

--- app/src/main/java/com/example/recipes/home/RecipeListAdapter.kt
+++ app/src/main/java/com/example/recipes/home/RecipeListAdapter.kt
@@ -9,13 +9,15 @@ import com.example.recipes.databinding.ItemListRecipeBinding
 class RecipeListAdapter : RecyclerView.Adapter<RecipeListAdapter.ViewHolder>() {
     inner class ViewHolder(private val binding: ItemListRecipeBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind() {
-
+            Glide.with(binding.root.context)
+                .load("https://spoonacular.com/recipeImages/661886-312x231.jpg")
+                .into(binding.imgRecipe)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = ItemListRecipeBinding.inflate(inflater)
+        val view = ItemListRecipeBinding.inflate(inflater, parent, false)
         return ViewHolder(view)
     }
 
