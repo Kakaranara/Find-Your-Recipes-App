1f36bb0d5da243be630b713c7ed0bb249cea6ef4
kakaranara <wahyukoco535@gmail.com>
2023-01-10 17:30:37 +0700

    adding local data source and refactor a bit

--- core/src/main/java/com/wahyu/recipes/core/data/local/LocalDataSource.kt
+++ core/src/main/java/com/wahyu/recipes/core/data/local/LocalDataSource.kt
@@ -1,4 +1,24 @@
 package com.wahyu.recipes.core.data.local
 
-class LocalDataSource {
+import com.wahyu.recipes.core.data.local.database.RecipesDao
+import com.wahyu.recipes.core.data.local.entity.FavoriteEntity
+import com.wahyu.recipes.core.data.local.entity.RecipeInformationEntity
+import com.wahyu.recipes.core.data.local.entity.RecipesEntity
+import kotlinx.coroutines.flow.Flow
+
+class LocalDataSource(
+    private val dao: RecipesDao,
+) {
+    fun getRecipes(): Flow<List<RecipesEntity>> = dao.getRecipes()
+    fun getRecipesInformation(id: Int): Flow<RecipeInformationEntity> = dao.getRecipeInformation(id)
+    suspend fun saveRecipes(recipeList: List<RecipesEntity>) = dao.saveRecipes(recipeList)
+    suspend fun saveRecipesInformation(recipe: RecipeInformationEntity) =
+        dao.saveRecipeInformation(recipe)
+
+    //? favorite
+
+    fun getFavoriteRecipes(): Flow<List<FavoriteEntity>> = dao.getFavoriteRecipe()
+    suspend fun saveToFavorite(favoriteEntity: FavoriteEntity) = dao.saveToFavorite(favoriteEntity)
+    suspend fun deleteFavorite(favoriteEntity: FavoriteEntity) = dao.deleteFavorite(favoriteEntity)
+    suspend fun deleteAllFavorite() = dao.deleteAllFavorite()
 }
--- core/src/main/java/com/wahyu/recipes/core/data/local/database/RecipesDao.kt
+++ core/src/main/java/com/wahyu/recipes/core/data/local/database/RecipesDao.kt
@@ -1,6 +1,7 @@
 package com.wahyu.recipes.core.data.local.database
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
@@ -15,18 +16,26 @@ interface RecipesDao {
     fun getRecipes(): Flow<List<RecipesEntity>>
 
     @Query("SELECT * FROM detail where id = :recipesId")
-    fun getRecipesInformation(recipesId: Int): Flow<RecipeInformationEntity>
+    fun getRecipeInformation(recipesId: Int): Flow<RecipeInformationEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun saveRecipes(recipeEntity: RecipesEntity)
+    suspend fun saveRecipes(recipeEntity: List<RecipesEntity>)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun saveRecipesInformation(recipesInformation: RecipeInformationEntity)
+    suspend fun saveRecipeInformation(recipesInformation: RecipeInformationEntity)
+
+    //? favorites
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun saveToFavorite(favoriteEntity: FavoriteEntity)
 
+    @Delete
+    suspend fun deleteFavorite(favoriteEntity: FavoriteEntity)
+
+    @Query("DELETE FROM favorite")
+    suspend fun deleteAllFavorite()
+
     @Query("SELECT * FROM favorite")
-    fun getFavoriteRecipe() : Flow<List<FavoriteEntity>>
+    fun getFavoriteRecipe(): Flow<List<FavoriteEntity>>
 
 }
