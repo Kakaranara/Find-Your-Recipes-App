dbfdefbe7d6f65b64d370a134a8d286842006b87
kakaranara <wahyukoco535@gmail.com>
2023-01-16 14:33:14 +0700

    final touch (submission 1)

--- favorite/src/main/java/com/example/recipe/favorite/ui/FavoriteFragment.kt
+++ favorite/src/main/java/com/example/recipe/favorite/ui/FavoriteFragment.kt
@@ -81,8 +81,8 @@ class FavoriteFragment : Fragment() {
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onDestroyView() {
+        super.onDestroyView()
         _binding = null
     }
 }
