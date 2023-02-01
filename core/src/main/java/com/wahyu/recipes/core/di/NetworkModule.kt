2fd6627d1e010ac42837c9efd30a22cac16f6fd7
kakaranara <wahyukoco535@gmail.com>
2023-02-01 13:46:40 +0700

    feat : pinning certificate with okHttp

--- core/src/main/java/com/wahyu/recipes/core/di/NetworkModule.kt
+++ core/src/main/java/com/wahyu/recipes/core/di/NetworkModule.kt
@@ -5,6 +5,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -18,8 +19,16 @@ class NetworkModule {
 
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
+        val host = "api.spoonacular.com"
+        val certPinner = CertificatePinner.Builder()
+            .add(host, "sha256/KktT3Oom4jItTYGySxE2lm8lf3JitTxH0slUYd8dxOY=")
+            .add(host, "sha256/FEzVOUp4dF3gI0ZVPRJhFbSJVXR+uQmMH65xhs1glH4=")
+            .add(host, "sha256/Y9mvm0exBk1JoQ57f9Vm28jKo5lFm/woKcVxrYxu80o=")
+            .build()
+
         return OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .certificatePinner(certPinner)
             .build()
     }
 
